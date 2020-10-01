d73e4432d6e9d598e7c0bee2b99fe3613916a6e3
Arnas Jelizarovas <jelizarovas@gmail.com>
2020-09-30 17:51:50 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,217 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import CountButton from "./components/CountButton";
 
 function App() {
+  const initialMaterials = [
+    {
+      name: "Small Box",
+      volume: 1.5,
+      w: "16 3/8",
+      d: "12 5/8",
+      h: "12 5/8",
+      count: 0,
+      img: "img/small.jpg",
+      history: {},
+    },
+    {
+      name: "Medium Box",
+      volume: 3.0,
+      w: "18",
+      d: "18",
+      h: "16",
+      count: 0,
+      img: "img/medium.jpg",
+      history: {},
+    },
+    {
+      name: "Large Box",
+      volume: 4.5,
+      w: "18",
+      d: "18",
+      h: "24",
+      count: 0,
+      img: "img/large.jpg",
+      history: {},
+    },
+    {
+      name: "Mattress Bag",
+      description: "For use on mattresses & box springs",
+      count: 0,
+      img: "img/matbags.jpg",
+      history: {},
+    },
+    {
+      name: "Wardrobe (Rent)",
+      volume: 13,
+      w: "24",
+      d: "24",
+      h: "40",
+
+      count: 0,
+      img: "img/wardrobe.jpg",
+      history: {},
+    },
+    {
+      name: "Wardrobe (Buy)",
+      volume: 13,
+      w: "24",
+      d: "24",
+      h: "40",
+
+      count: 0,
+      img: "img/wardrobe.jpg",
+      history: {},
+    },
+    {
+      name: "Laydown Wardrobe",
+      volume: 13,
+      w: "24",
+      d: "24",
+      h: "40",
+
+      count: 0,
+      img: "img/laydown.jpg",
+      history: {},
+    },
+    {
+      name: "Mirror Pack (Small)",
+      volume: 13,
+      w: "24",
+      d: "24",
+      h: "40",
+
+      count: 0,
+      img: "img/smirror.jpg",
+      history: {},
+    },
+    {
+      name: "Mirror Pack (Large)",
+      volume: 13,
+      w: "24",
+      d: "24",
+      h: "40",
+
+      count: 0,
+      img: "img/lmirror.jpg",
+      history: {},
+    },
+    {
+      name: "Paper Pads",
+      count: 0,
+      img: "img/pads.jpg",
+      history: {},
+    },
+    {
+      name: "Stretch Wrap",
+      count: 0,
+      img: "img/wrap.jpg",
+      history: {},
+    },
+    {
+      name: "Carpet Protection",
+      count: 0,
+      img: "img/carpet.jpg",
+      history: {},
+    },
+    {
+      name: "Ropes",
+      count: 0,
+      img: "img/rope.jpg",
+      history: {},
+    },
+    {
+      name: "Blankets",
+      count: 0,
+      img: "img/blanket.jpg",
+      history: {},
+    },
+  ];
+
+  const [materials, setMaterials] = useState(initialMaterials);
+
+  const Material = ({ m }) => {
+    const { name, volume, count, img, w, d, h, description, subtext } = m;
+
+    const [tooltip, setTooltip] = useState(false);
+
+    const onClick = () => {
+      setTooltip((t) => !t);
+    };
+    return (
+      <tr>
+        <td className="px-6 py-4 whitespace-no-wrap ">
+          <div className="flex items-center">
+            <div className="flex-shrink-0 h-10 w-10">
+              <img className="h-10 w-10 " src={img} alt="" />
+            </div>
+            <div className="ml-4">
+              <div className="text-sm leading-5 font-medium text-gray-900">
+                {name}{" "}
+                <div className={tooltip ? "mx-2 z-50 absolute " : "hidden"}>
+                  <div className="bg-black text-white text-xs rounded py-1 px-4 right-0 bottom-full   ">
+                    {w && d && h ? `W: ${w}", D: ${d}", H: ${h}" ` : description}
+                  </div>{" "}
+                </div>
+                <svg
+                  className="fill-current w-4 h-4 inline text-gray-500"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  onClick={onClick}
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+              <div className="text-sm leading-5 text-gray-500">
+                <span>{volume ? `${volume} c.u. ft.` : subtext} </span>
+              </div>
+            </div>
+          </div>
+        </td>
+
+        <td className="px-0 py-0 whitespace-no-wrap text-sm leading-5 text-gray-500">
+          <CountButton count={count} />
+        </td>
+      </tr>
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="max-w-lg rounded overflow-hidden shadow-lg mx-auto mt-2">
+      <div className="px-6 py-4 ">
+        <div className="font-bold text-orange-500 text-xl mb-0 text-center">Material Calculator</div>
+      </div>
+
+      <div className="flex flex-col">
+        <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+              <table className="min-w-full divide-y divide-gray-200">
+                <thead>
+                  <tr>
+                    <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                      Material
+                    </th>
+                    <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                      Count
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="bg-white divide-y divide-gray-200">
+                  {materials.map((m) => (
+                    <Material m={m} key={m.name + m.volume} />
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
---
+++ src/components/CountButton.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import useLongPress from "../utils/useLongPress";
+import clsx from "clsx";
+
+const CountButton = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
+  const decrement = () => {
+    setCount((c) => (c > 0 ? c - 1 : c));
+  };
+  const onChange = (e) => {
+    var val = parseInt(e.target.value) || 0;
+    return setCount(val);
+  };
+
+  const onLongPress = () => {
+    setCount(0);
+  };
+
+  const defaultOptions = {
+    shouldPreventDefault: true,
+    delay: 1000,
+  };
+
+  const longPressEvent = useLongPress(onLongPress, decrement, defaultOptions);
+
+  return (
+    <div className={`custom-number-input w-32 bg-gray-100 rounded-lg`}>
+      <div className="flex flex-row h-10 w-full rounded-lg relative bg-transparent mt-1">
+        <button
+          {...longPressEvent}
+          className="disable-select  text-gray-600 hover:text-red-700 hover:bg-red-200 h-full w-20 rounded-l cursor-pointer outline-none"
+        >
+          <span className="m-auto text-2xl font-thin">−</span>
+        </button>
+        <input
+          min="0"
+          type="number"
+          className={clsx(
+            "outline-none bg-gray-100 focus:outline-none text-center w-full  font-semibold text-md hover:text-black focus:text-black  md:text-basecursor-default flex items-center text-gray-700  outline-none",
+            count > 0 && "bg-yellow-300"
+          )}
+          name="custom-input-number"
+          onChange={onChange}
+          onKeyPress={(e) => {
+            if (
+              e.key === "ArrowLeft" ||
+              e.key === "ArrowRight" ||
+              e.key === "ArrowUp" ||
+              e.key === "ArrowDown" ||
+              e.key === "Delete" ||
+              e.key === "Backspace"
+            ) {
+              return;
+            } else if (e.key.search(/\d/) === -1) {
+              e.preventDefault();
+            }
+          }}
+          onKeyDown={(e) => (e.key === "Enter" ? e.target.blur() : null)}
+          value={count}
+        ></input>
+        <button
+          onClick={increment}
+          className=" text-gray-600 hover:text-green-700 hover:bg-green-200 h-full w-20 rounded-r cursor-pointer"
+        >
+          <span className="m-auto text-2xl font-thin">+</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CountButton;
