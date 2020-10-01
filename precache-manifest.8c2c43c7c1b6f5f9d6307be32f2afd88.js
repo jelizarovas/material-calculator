e40aca8dd6c8ac807c22861a0400d6d7fe88ca4f
Arnas Jelizarovas <jelizarovas@gmail.com>
2020-09-30 19:04:24 -0700

    Updates

---
+++ precache-manifest.8c2c43c7c1b6f5f9d6307be32f2afd88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81c51ee5d35574ac4b4730e7e3c2a52e",
+    "url": "/material-calculator/index.html"
+  },
+  {
+    "revision": "b01c9df72f73a21b1789",
+    "url": "/material-calculator/static/css/main.36a50ae3.chunk.css"
+  },
+  {
+    "revision": "5d6ee672629bcae5ab1c",
+    "url": "/material-calculator/static/js/2.a5be7cdb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/material-calculator/static/js/2.a5be7cdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b01c9df72f73a21b1789",
+    "url": "/material-calculator/static/js/main.e1efc7ff.chunk.js"
+  },
+  {
+    "revision": "a95287e493c2b972f06e",
+    "url": "/material-calculator/static/js/runtime-main.e8a12f00.js"
+  }
+]);
