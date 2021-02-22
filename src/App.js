1dd9f71e15e2e62d962ca0094c963828ccabf1eb
Arnas Jelizarovas <jelizarovas@gmail.com>
2021-02-21 21:25:37 -0800

    add component navbar

--- src/App.js
+++ src/App.js
@@ -16,55 +16,21 @@ import {
   HashRouter as Router,
   Switch,
   Route,
-  Link,
   Redirect,
 } from "react-router-dom";
 import { Rates } from "./components/Rates";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
       <InventoryProvider>
         <Router basename="/">
-          <div className="flex-row justify-center w-screen bg-gray-800 ">
-            <div className="text-white p-4 bg-black flex  justify-around">
-              <div className="flex cursor-pointer">
-                <img
-                  className="bg-white w-6 h-6 p-1"
-                  src={`${process.env.PUBLIC_URL}/favicon.ico`}
-                  alt=""
-                />
-                <span className="px-2">
-                  SFM<strong>tools</strong>
-                </span>
-              </div>
-              <nav className="px-5 ">
-                <ul className="flex justify-around">
-                  <li className="px-2">
-                    <Link to="/client">Client</Link>
-                  </li>
-                  <li className="px-2">
-                    <Link to="/rates">Rates</Link>
-                  </li>
-                  <li className="px-2">
-                    <Link to="/bol">BOL</Link>
-                  </li>
-                  <li className="px-2">
-                    <Link to="/inventory">Inventory</Link>
-                  </li>
-                  <li className="px-2">
-                    <Link to="/estimate">Estimate</Link>
-                  </li>
-                  <li className="px-2">
-                    <Link to="/materials">Materials</Link>
-                  </li>
-                  <li className="px-2">
-                    <Link to="/overview">Overview</Link>
-                  </li>
-                </ul>
-              </nav>
-            </div>
-          </div>
+          {/* <div className="flex-row justify-center w-screen bg-gray-800 "> */}
+          {/* <div className="text-white p-4 bg-black flex  justify-around"> */}
+          <Navbar />
+          {/* </div> */}
+          {/* </div> */}
 
           <ClientProvider>
             <Switch>
---
+++ src/components/Navbar.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Menu, Clear } from "@material-ui/icons";
+
+export default function Navbar({ fixed }) {
+  const [navbarOpen, setNavbarOpen] = React.useState(false);
+  return (
+    <>
+      <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-black mb-3">
+        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+
+            <div className="flex cursor-pointer">
+              <img
+                className="bg-white w-6 h-6 p-1"
+                src={`${process.env.PUBLIC_URL}/favicon.ico`}
+                alt=""
+              />
+              <span className="px-2 text-white">
+                <strong>SFM</strong>
+              </span>
+            </div>
+            <button
+              className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+              type="button"
+              onClick={() => setNavbarOpen(!navbarOpen)}
+            >
+              {navbarOpen ? <Clear /> : <Menu />}
+            </button>
+          </div>
+          <div
+            className={
+              "lg:flex flex-grow items-center" +
+              (navbarOpen ? " flex" : " hidden")
+            }
+            id="example-navbar-danger"
+          >
+            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+<NavLink to="client" text="Client" />
+<NavLink to="rates" text="rates" />
+<NavLink to="bol" text="BOL" />
+<NavLink to="inventory" text="inventory" />
+<NavLink to="estimate" text="estimate" />
+<NavLink to="materials" text="materials" />
+<NavLink to="overview" text="overview" />
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+const NavLink = ({to, text}) => {
+
+    return (
+        <li className="px-2 nav-item">
+            <Link className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75 ml-2" to={to}>{text}</Link>
+        </li>)
+}
