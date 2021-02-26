ae38d6731c34796365737884d439f3cbe8e35122
Arnas Jelizarovas <jelizarovas@gmail.com>
2021-02-25 22:12:16 -0800

    added gaps countbutton for better touch ux

--- src/components/CountButton.js
+++ src/components/CountButton.js
@@ -31,47 +31,45 @@ const CountButton = ({ count, changeCount }) => {
   const longPressEvent = useLongPress(onLongPress, decrement, defaultOptions);
 
   return (
-    <div className={`custom-number-input w-32 bg-gray-100 bg-opacity-50  rounded-lg`}>
-      <div className="flex flex-row h-10 w-full rounded-lg relative bg-transparent ">
-        <button
-          {...longPressEvent}
-          className="disable-select  text-gray-600 hover:text-red-700 hover:bg-red-200 h-full w-20 rounded-l cursor-pointer outline-none"
-        >
-          <span className="m-auto text-2xl font-thin">−</span>
-        </button>
-        <input
-          min="0"
-          type="number"
-          name="custom-input-number"
-          className={clsx(
-            "outline-none bg-gray-100 bg-opacity-50 focus:outline-none text-center w-full  font-semibold text-md hover:text-black focus:text-black  md:text-basecursor-default flex items-center text-gray-700",
-            count > 0 && "bg-yellow-300"
-          )}
-          onChange={onChange}
-          onKeyPress={(e) => {
-            if (
-              e.key === "ArrowLeft" ||
-              e.key === "ArrowRight" ||
-              e.key === "ArrowUp" ||
-              e.key === "ArrowDown" ||
-              e.key === "Delete" ||
-              e.key === "Backspace"
-            ) {
-              return;
-            } else if (e.key.search(/\d/) === -1) {
-              e.preventDefault();
-            }
-          }}
-          onKeyDown={(e) => (e.key === "Enter" ? e.target.blur() : null)}
-          value={count}
-        ></input>
-        <button
-          onClick={increment}
-          className=" text-gray-600 hover:text-green-700 hover:bg-green-200 h-full w-20 rounded-r cursor-pointer"
-        >
-          <span className="m-auto text-2xl font-thin">+</span>
-        </button>
-      </div>
+    <div className="flex flex-row w-28 h-8  rounded-md relative bg-gray-100 bg-opacity-50 my-1 ">
+      <button
+        {...longPressEvent}
+        className="disable-select leading-4 p-2 text-gray-600 hover:text-red-700 hover:bg-red-200   w-10 rounded-l cursor-pointer outline-none"
+      >
+        <span className="m-auto text-2xl   font-thin  ">−</span>
+      </button>
+      <input
+        min="0"
+        type="number"
+        name="custom-input-number"
+        className={clsx(
+          "outline-none bg-gray-100 bg-opacity-50 focus:outline-none text-center w-12  font-semibold text-md hover:text-black focus:text-black  md:text-basecursor-default flex items-center text-gray-700",
+          count > 0 && "bg-yellow-300"
+        )}
+        onChange={onChange}
+        onKeyPress={(e) => {
+          if (
+            e.key === "ArrowLeft" ||
+            e.key === "ArrowRight" ||
+            e.key === "ArrowUp" ||
+            e.key === "ArrowDown" ||
+            e.key === "Delete" ||
+            e.key === "Backspace"
+          ) {
+            return;
+          } else if (e.key.search(/\d/) === -1) {
+            e.preventDefault();
+          }
+        }}
+        onKeyDown={(e) => (e.key === "Enter" ? e.target.blur() : null)}
+        value={count}
+      ></input>
+      <button
+        onClick={increment}
+        className="leading-4 p-2 text-gray-600 hover:text-green-700 hover:bg-green-200 h-full w-20 rounded-r cursor-pointer"
+      >
+        <span className="m-auto text-2xl font-thin">+</span>
+      </button>
     </div>
   );
 };
