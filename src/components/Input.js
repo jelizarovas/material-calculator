3b9524bd150ee24e92361253fa2f001ddfee9348
Arnas Jelizarovas <jelizarovas@gmail.com>
2021-02-27 22:58:41 -0800

    fixes #17

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 
 import { Clear } from "@material-ui/icons/";
 
@@ -16,12 +16,20 @@ export const Input = (props) => {
     clear = true,
   } = props;
 
+  // const [isFocused, setIsFocused] = useState(false);
+
+  // const onBlur = () => setIsFocused(false);
+  // const onFocus = () => setIsFocused(true);
+
+  const inputRef = useRef();
+
   return (
     <div className="relative text-gray-600 focus-within:text-red-400 w-100 m-2">
       <span className="absolute inset-y-0 left-0 flex items-center pl-2">
         <Icon />
       </span>
       <input
+        ref={inputRef}
         type={type}
         name={name}
         className=" w-full  py-2 pr-6 text-sm text-black bg-white rounded-md pl-12 focus:outline-none focus:bg-white focus:text-gray-900"
@@ -32,11 +40,16 @@ export const Input = (props) => {
         step={step}
         readOnly={readOnly}
         inputmode={inputmode}
+        // onFocus={onFocus}
+        // onBlur={onBlur}
       />
       {clear && (value.length > 0 || (type === "number" && value > 0)) ? (
         <span
           className="absolute inset-y-0 right-0 flex items-center px-2 cursor-pointer opacity-20 focus:opacity-100 hover:opacity-100"
-          onClick={() => onChange({ target: { name, value: "" } })}
+          onClick={() => {
+            onChange({ target: { name, value: "" } });
+            inputRef.current.focus();
+          }}
         >
           <Clear />
         </span>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,13 @@
 import React from "react";
 import { Link, useLocation, useHistory } from "react-router-dom";
-import { Menu, Clear, TurnedIn, TurnedInNot, NavigateNext, NavigateBefore } from "@material-ui/icons";
+import {
+  Menu,
+  Clear,
+  TurnedIn,
+  TurnedInNot,
+  NavigateNext,
+  NavigateBefore,
+} from "@material-ui/icons";
 
 export default function Navbar({ fixed }) {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
@@ -9,11 +16,19 @@ export default function Navbar({ fixed }) {
   let { pathname } = useLocation();
   let history = useHistory();
 
-  const steps = ["client", "rates", "inventory", "estimate", "materials", "overview"];
+  const steps = [
+    "client",
+    "rates",
+    "inventory",
+    "estimate",
+    "materials",
+    "overview",
+  ];
   const currentIndex = steps.indexOf(pathname.substring(1));
 
   const nextStep = () => {
-    if (steps.length - 1 > currentIndex) history.push("/" + steps[currentIndex + 1]);
+    if (steps.length - 1 > currentIndex)
+      history.push("/" + steps[currentIndex + 1]);
   };
 
   const previousStep = () => {
@@ -38,12 +53,16 @@ export default function Navbar({ fixed }) {
   };
 
   return (
-    <nav className=" fixed inset-x-0 bottom-0 z-10  flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-black ">
+    <nav className=" fixed inset-x-0 bottom-0 z-100  flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-black ">
       {/* <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-black "> */}
       <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
         <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
           <div className="flex text-white   cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent lg:hidden outline-none focus:outline-none ">
-            <img className="bg-white w-6 h-6 p-1" src={`${process.env.PUBLIC_URL}/favicon.ico`} alt="" />
+            <img
+              className="bg-white w-6 h-6 p-1"
+              src={`${process.env.PUBLIC_URL}/favicon.ico`}
+              alt=""
+            />
             <span className="px-2 text-white">
               <strong>SFM</strong>
             </span>
@@ -83,7 +102,10 @@ export default function Navbar({ fixed }) {
           </div>
         </div>
         <div
-          className={"lg:flex flex-grow items-center" + (navbarOpen ? " flex" : " hidden")}
+          className={
+            "lg:flex flex-grow items-center" +
+            (navbarOpen ? " flex" : " hidden")
+          }
           id="example-navbar-danger"
         >
           <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
