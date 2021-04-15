85aa0b6a6a1f4d2d639d64a4abc3e4155558e358
Arnas Jelizarovas <jelizarovas@gmail.com>
2021-04-14 21:39:34 -0700

    added few fields for make text

--- src/components/Tools/MakeText.js
+++ src/components/Tools/MakeText.js
@@ -2,19 +2,31 @@ import { AccessTime, CreditCard, DirectionsWalk, Phone } from "@material-ui/icon
 import React, { useState } from "react";
 import { ButtonSelect } from "../Inputs/ButtonSelect";
 import { Input } from "../Inputs/Input";
+import { useLocation } from "react-router-dom";
+
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
 
 export const MakeText = () => {
-  const [phone, setPhone] = useState("");
-  const [customer, setCustomer] = useState("");
-  const [greeting, setGreeting] = useState("Good morning");
+  let query = useQuery();
+
+  const [phone, setPhone] = useState(query.get("p") || query.get("phone") || "");
+  const [customer, setCustomer] = useState(query.get("n") || query.get("name") || "");
+  const [greeting, setGreeting] = useState(isNowAMPM() === "AM" ? "Good Morning" : "Good afternoon");
   const [crewLead, setCrewLead] = useState("Arnas");
-  const [company, setcompany] = useState("Paramount Transportation Systems");
+  const [company, setCompany] = useState("Paramount Transportation Systems");
+  const [moveType, setMoveType] = useState("delivering");
+  const [scheduledETA, setScheduledETA] = useState("1 p.m. to 4 p.m.");
   const onChangePhone = (e) => setPhone(e.target.value);
   const onChangeCustomer = (e) => setCustomer(e.target.value);
   const onChangeGreeting = (e) => setGreeting(e.target.value);
+  const onChangeCompany = (e) => setCompany(e.target.value);
+  const onChangeScheduledETA = (e) => setScheduledETA(e.target.value);
+  const onChangeMoveType = (e) => setMoveType(e.target.value);
 
   return (
-    <div>
+    <div className="bg-gray-100 p-2 rounded-b-lg">
       <Input name="phone" Icon={Phone} value={phone} onChange={onChangePhone} placeholder="Text phone number" />
 
       <Input
@@ -32,12 +44,43 @@ export const MakeText = () => {
           buttons={[
             { value: "Good morning", placeholder: "Morning", Icon: AccessTime },
             { value: "Good afternoon", placeholder: "Afternoon", Icon: CreditCard },
+            { value: "Hello", placeholder: "Neutral", Icon: CreditCard },
+          ]}
+        />
+      </div>
+      <div className="bg-white mx-2 py-1 rounded-md">
+        <ButtonSelect
+          name="moveType"
+          value={moveType}
+          onClick={onChangeMoveType}
+          buttons={[
+            { value: "delivering your goods ", placeholder: "Delivery", Icon: AccessTime },
+            { value: "packing up your goods ", placeholder: "Origin", Icon: CreditCard },
+            { value: "moving you", placeholder: "Local", Icon: CreditCard },
+          ]}
+        />
+      </div>
+      <div className="bg-white mx-2 py-1 rounded-md">
+        <ButtonSelect
+          name="company"
+          value={company}
+          onClick={onChangeCompany}
+          buttons={[
+            { value: "Paramount Transportation Systems", placeholder: "Paramount", Icon: AccessTime },
+            { value: "SuperFriends Moving", placeholder: "SuperFriends", Icon: CreditCard },
           ]}
         />
       </div>
       <div className="eta-window"></div>
 
-      <div className="bg-white p-10 my-2 rounded-lg">{`${greeting} ${customer}, this is Arnas with ${company}`}</div>
+      <div className="bg-white p-10 my-2 rounded-lg">{`${greeting} ${customer}, this is Arnas with ${company}. We'll be ${moveType} today. Our scheduled ETA is ${scheduledETA}. Please do not hesitate to text me or call me if you have any questions. `}</div>
+      <button className="bg-green-500 text-white p-2 px-4 rounded-md" type="submit">
+        Send text
+      </button>
     </div>
   );
 };
+
+function isNowAMPM() {
+  return new Date().getHours() >= 12 ? "PM" : "AM";
+}
--- src/components/Tools/Tools.js
+++ src/components/Tools/Tools.js
@@ -18,6 +18,10 @@ export const Tools = () => {
         <div className="bg-gray-500" style={{ minWidth: "265px", minHeight: "265px" }}></div>
         <Link to="/t/coordinates">ABHS</Link>
       </div>
+      <div className="m-5 p-2 border-2 rounded-md relative flex justify-center flex-col">
+        <div className="bg-gray-500" style={{ minWidth: "265px", minHeight: "265px" }}></div>
+        <Link to="/t/sms">Text Customer</Link>
+      </div>
     </div>
   );
 };
