7b367aaffafdbc9c729642bdecf4a353a4cf8e0a
Arnas Jelizarovas <jelizarovas@gmail.com>
2021-04-17 21:55:34 -0700

    added reducer to make text

--- src/components/Tools/MakeText.js
+++ src/components/Tools/MakeText.js
@@ -1,5 +1,5 @@
 import { AccessTime, CreditCard, Delete, DirectionsWalk, Edit, Message, Phone, Save } from "@material-ui/icons";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import { ButtonSelect } from "../Inputs/ButtonSelect";
 import { Input } from "../Inputs/Input";
 import { useHistory, useLocation } from "react-router-dom";
@@ -14,39 +14,48 @@ function useQuery() {
   return new URLSearchParams(useLocation().search);
 }
 
+const reducer = (state, action) => {
+  console.log("reducer");
+
+  switch (action.type) {
+    case "update":
+      return { ...state, ...action.payload };
+    case "clear":
+      return {};
+
+    default:
+      break;
+  }
+  return { ...state };
+};
+
 export const MakeText = () => {
   let query = useQuery();
   const firestore = useFirestore();
-  const [phone, setPhone] = useState(query.get("p") || query.get("phone") || "");
-  const [customer, setCustomer] = useState(query.get("n") || query.get("name") || "");
-  const [greeting, setGreeting] = useState(isNowAMPM() === "AM" ? "Good Morning" : "Good afternoon");
-  const [crewLead, setCrewLead] = useState("Arnas");
-  const [company, setCompany] = useState("Paramount Transportation Systems");
-  const [moveType, setMoveType] = useState("delivering");
-  const [scheduledETA, setScheduledETA] = useState("1 p.m. to 4 p.m.");
-  const [sms, setSms] = useState("");
-  const [editId, setEditId] = useState("");
 
-  const onChangePhone = (e) => setPhone(e.target.value);
-  const onChangeCustomer = (e) => setCustomer(e.target.value);
-  const onChangeGreeting = (e) => setGreeting(e.target.value);
-  const onChangeCompany = (e) => setCompany(e.target.value);
-  const onChangeScheduledETA = (e) => setScheduledETA(e.target.value);
-  const onChangeMoveType = (e) => setMoveType(e.target.value);
+  const initialState = {
+    phone: query.get("p") || query.get("phone") || "",
+    customer: query.get("n") || query.get("name") || "",
+    greeting: isNowAMPM() === "AM" ? "Good Morning" : "Good afternoon",
+    crewLead: "Arnas",
+    company: "Paramount Transportation Systems",
+    moveType: "delivering",
+    scheduledETA: "1 p.m. to 4 p.m.",
+    sms: "",
+    editId: "",
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const { phone, customer, greeting, crewLead, company, moveType, scheduledETA, sms, editId } = state;
+
+  const onChange = (e) => dispatch({ type: "update", payload: { [e.target.name]: e.target.value } });
 
   const handleEdit = (id, payload) => {
     return function (e) {
-      console.log(id, payload);
-      setEditId(id);
-      if (!!payload) return null;
-      if (payload.phone) setPhone(payload.phone);
-      if (payload.customer) setCustomer(payload.customer);
-      if (payload.greeting) setGreeting(payload.greeting);
-      if (payload.crewLead) setCrewLead(payload.crewLead);
-      if (payload.company) setCompany(payload.company);
-      if (payload.moveType) setMoveType(payload.moveType);
-      if (payload.scheduledETA) setScheduledETA(payload.scheduledETA);
-      if (payload.sms) setSms(payload.sms);
+      console.log({ id, payload });
+      // if (!!payload) return null;
+      dispatch({ type: "update", payload: { editId: id, ...payload } });
     };
   };
 
@@ -54,38 +63,43 @@ export const MakeText = () => {
     const payload = { phone, customer, greeting, crewLead, company, moveType, scheduledETA, sms };
     if (!!editId) {
       const tempId = editId;
-      setEditId("");
+      dispatch({ type: "update", payload: { editId: "" } });
+
       return firestore.collection("sms").doc(tempId).update(payload);
     }
     return firestore.collection("sms").add(payload);
   }
 
   useEffect(() => {
-    setSms(
-      `${greeting}${
-        !!customer ? " " + customer : ""
-      }, this is Arnas with ${company}. We'll be ${moveType} today. Our scheduled ETA is ${scheduledETA}. If you have any questions please text me or call me. `
-    );
+    dispatch({
+      type: "update",
+      payload: {
+        sms: `${greeting}${
+          !!customer ? " " + customer : ""
+        }, this is Arnas with ${company}. We'll be ${moveType} today. Our scheduled ETA is ${scheduledETA}. If you have any questions please text me or call me. `,
+      },
+    });
+
     return () => {};
   }, [phone, customer, greeting, crewLead, company, moveType, scheduledETA]);
 
   return (
     <>
       <div className="bg-gray-100 p-2 rounded-b-lg">
-        <Input name="phone" Icon={Phone} value={phone} onChange={onChangePhone} placeholder="Text phone number" />
+        {!!editId && (
+          <>
+            <span>Editing: {editId}</span>
+            <button onClick={() => dispatch({ type: "clear" })}>x</button>
+          </>
+        )}
+        <Input name="phone" Icon={Phone} value={phone} onChange={onChange} placeholder="Text phone number" />
 
-        <Input
-          name="phone"
-          Icon={DirectionsWalk}
-          value={customer}
-          onChange={onChangeCustomer}
-          placeholder="Customer name"
-        />
+        <Input name="customer" Icon={DirectionsWalk} value={customer} onChange={onChange} placeholder="Customer name" />
         <div className="bg-white mx-2 py-1 rounded-md">
           <ButtonSelect
             name="greeting"
             value={greeting}
-            onClick={onChangeGreeting}
+            onClick={onChange}
             buttons={[
               { value: "Good morning", placeholder: "Morning", Icon: AccessTime },
               { value: "Good afternoon", placeholder: "Afternoon", Icon: CreditCard },
@@ -97,7 +111,7 @@ export const MakeText = () => {
           <ButtonSelect
             name="moveType"
             value={moveType}
-            onClick={onChangeMoveType}
+            onClick={onChange}
             buttons={[
               { value: "delivering your goods ", placeholder: "Delivery", Icon: AccessTime },
               { value: "packing up your goods ", placeholder: "Origin", Icon: CreditCard },
@@ -109,7 +123,7 @@ export const MakeText = () => {
           <ButtonSelect
             name="company"
             value={company}
-            onClick={onChangeCompany}
+            onClick={onChange}
             buttons={[
               { value: "Paramount Transportation Systems", placeholder: "Paramount", Icon: AccessTime },
               { value: "SuperFriends Moving", placeholder: "SuperFriends", Icon: CreditCard },
@@ -176,9 +190,11 @@ const ListSMS = ({ handleEdit }) => {
 const SMSLink = (props) => {
   const { id, handleEdit, ...rest } = props;
   return (
-    <div className="overflow-x-hidden w-full p-2 flex justify-around">
-      <div>Date</div>
+    <div className=" w-full p-2 grid grid-flow-col auto-cols-fr justify-items-center text-xs ">
+      {/* <div>Date</div> */}
       <div>{rest?.customer}</div>
+      <div>{rest?.phone}</div>
+      <div>{rest?.moveType}</div>
       <div>{id}</div>
       <div className="flex">
         <div className="px-2 cursor-pointer" onClick={handleEdit(id, rest)}>
