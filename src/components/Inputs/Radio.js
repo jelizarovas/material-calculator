28249fb9fd067b93cadb7fde335f370d7aa02974
Arnas Jelizarovas <jelizarovas@gmail.com>
2021-05-11 21:49:42 -0700

    fixes #25 radio toString error

--- src/components/Inputs/Radio.js
+++ src/components/Inputs/Radio.js
@@ -14,7 +14,7 @@ function isObjempty(obj) {
 }
 
 export function Radio(props) {
-  const { showPrice = false, options = {}, groupName = "", row = false, dense = false } = props;
+  const { showPrice = false, options = [], groupName = "", row = false, dense = false } = props;
   const [selected, setSelected] = useState(options[0].value);
 
   if (isObjempty(options))
@@ -32,13 +32,14 @@ export function Radio(props) {
             {groupName}
           </RadioGroup.Label>
           <div className={`space-y-2 flex ${row ? "flex-row justify-around space-x-2" : "flex-col"}`}>
-            {options.map((option) => (
-              <RadioGroup.Option
-                key={option.name}
-                value={option.value}
-                disabled={option.disabled}
-                className={({ active, checked, disabled }) =>
-                  `${active ? "ring-2 ring-offset-2 ring-offset-purple-300 ring-white ring-opacity-60" : ""}
+            {options &&
+              options.map((option) => (
+                <RadioGroup.Option
+                  key={option.name}
+                  value={option.value}
+                  disabled={option.disabled}
+                  className={({ active, checked, disabled }) =>
+                    `${active ? "ring-2 ring-offset-2 ring-offset-purple-300 ring-white ring-opacity-60" : ""}
                   ${
                     checked
                       ? "bg-purple-900 border-purple-700 bg-opacity-75 "
@@ -49,35 +50,37 @@ export function Radio(props) {
                   border-b-2 relative rounded-lg  ${
                     !!dense ? "px-2 py-1" : "px-4 py-3"
                   } w-full overflow-hidden cursor-pointer flex focus:outline-none ${
-                    checked ? "text-white shadow-md" : disabled ? "text-gray-400 shadow-md" : "text-gray-900"
-                  }`
-                }
-              >
-                {({ active, checked, disabled }) => (
-                  <>
-                    <div className={`flex items-center justify-between w-full `}>
-                      <div className={`flex items-center  truncate mr-0 `}>
-                        {option.Icon && <option.Icon className="mr-2 " />}
-                        <RadioGroup.Label as="p" className="font-medium ">
-                          {option.name}
-                        </RadioGroup.Label>
+                      checked ? "text-white shadow-md" : disabled ? "text-gray-400 shadow-md" : "text-gray-900"
+                    }`
+                  }
+                >
+                  {({ active, checked, disabled }) => (
+                    <>
+                      <div className={`flex items-center justify-between w-full `}>
+                        <div className={`flex items-center  truncate mr-0 `}>
+                          {option.Icon && <option.Icon className="mr-2 " />}
+                          <RadioGroup.Label as="p" className="font-medium ">
+                            {option.name}
+                          </RadioGroup.Label>
+                        </div>
+                        <div className="flex items-center">
+                          {showPrice && (
+                            <RadioGroup.Description
+                              as="span"
+                              className={`whitespace-nowrap ${!!checked ? "text-purple-100" : "text-gray-500"}`}
+                            >
+                              {option && option.price && isNaN(option.price) ? option.price : `$ ${option?.price}`}
+                            </RadioGroup.Description>
+                          )}
+                          <div className="flex-shrink-0 text-white w-6 h-6 ml-4">
+                            {checked && <Check className="" />}
+                          </div>
+                        </div>
                       </div>
-                      <div className="flex items-center">
-                        {showPrice && (
-                          <RadioGroup.Description
-                            as="span"
-                            className={`whitespace-nowrap ${checked ? "text-purple-100" : "text-gray-500"}`}
-                          >
-                            {option.price && isNaN(option.price) ? option.price : "$ " + option.price.toString()}
-                          </RadioGroup.Description>
-                        )}
-                        <div className="flex-shrink-0 text-white w-6 h-6 ml-4">{checked && <Check className="" />}</div>
-                      </div>
-                    </div>
-                  </>
-                )}
-              </RadioGroup.Option>
-            ))}
+                    </>
+                  )}
+                </RadioGroup.Option>
+              ))}
           </div>
         </RadioGroup>
       </div>
