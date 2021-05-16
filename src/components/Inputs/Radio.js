6fcf2f8edf673382d4d6fea9b871534f197831bc
Arnas Jelizarovas <jelizarovas@gmail.com>
2021-05-16 16:36:10 -0700

    restyled radio buttons

--- src/components/Inputs/Radio.js
+++ src/components/Inputs/Radio.js
@@ -2,19 +2,22 @@ import { useState } from "react";
 import { RadioGroup } from "@headlessui/react";
 import { Check, Warning } from "@material-ui/icons";
 
-// const defaultOptions = [
-//   { value: "local", name: "Local (Hourly)", Icon: AccessTime, price: "$24.00" },
-//   { value: "flatRate", name: "Flat Rate", Icon: CreditCard, price: "$1,354.99" },
-//   { value: "longDistance", name: "Long Distance", Icon: LocalShipping, price: "$10,000.03", disabled: true },
-// ];
-
 function isObjempty(obj) {
   for (var i in obj) return false;
   return true;
 }
 
 export function Radio(props) {
-  const { showPrice = false, options = [], groupName = "", row = false, dense = false } = props;
+  const {
+    showPrice = false,
+    options = [],
+    groupName = "",
+    row = false,
+    dense = false,
+    //TODO SYNC SELECTED WITH DB
+    // value = {},
+    // setValue = () => {},
+  } = props;
   const [selected, setSelected] = useState(options[0].value);
 
   if (isObjempty(options))
@@ -25,65 +28,56 @@ export function Radio(props) {
     );
 
   return (
-    <div className="w-full py-0 max-w-full overflow-hidden text-xs ">
-      <div className="w-full max-w-md mx-auto">
-        <RadioGroup value={selected} onChange={setSelected}>
-          <RadioGroup.Label className="text-black mx-auto w-full flex justify-center my-1">
-            {groupName}
-          </RadioGroup.Label>
-          <div className={`space-y-2 flex ${row ? "flex-row justify-around space-x-2" : "flex-col"}`}>
-            {options &&
-              options.map((option) => (
-                <RadioGroup.Option
-                  key={option.name}
-                  value={option.value}
-                  disabled={option.disabled}
-                  className={({ active, checked, disabled }) =>
-                    `${active ? "ring-2 ring-offset-2 ring-offset-purple-300 ring-white ring-opacity-60" : ""}
-                  ${
-                    checked
-                      ? "bg-purple-900 border-purple-700 bg-opacity-75 "
-                      : disabled
-                      ? "bg-transparent"
-                      : "bg-white"
-                  }
-                  border-b-2 relative rounded-lg  ${
-                    !!dense ? "px-2 py-1" : "px-4 py-3"
-                  } w-full overflow-hidden cursor-pointer flex focus:outline-none ${
-                      checked ? "text-white shadow-md" : disabled ? "text-gray-400 shadow-md" : "text-gray-900"
-                    }`
-                  }
-                >
-                  {({ active, checked, disabled }) => (
+    <RadioGroup
+      value={selected}
+      onChange={setSelected}
+      className="w-full  mx-auto pt-2 text-xs text-gray-900 select-none"
+    >
+      {groupName && (
+        <RadioGroup.Label className="text-black mx-auto w-full flex justify-center my-1">{groupName}</RadioGroup.Label>
+      )}
+      <div className={` flex ${row ? "flex-row justify-around space-x-2" : "flex-col space-y-2 max-w-md mx-auto"}`}>
+        {options.map((option) => (
+          <RadioGroup.Option
+            key={option.name}
+            value={option.value}
+            disabled={option.disabled}
+            className={({ active, checked, disabled }) =>
+              `w-full  border-b-2 rounded-lg   cursor-pointer focus:outline-none flex items-center justify-between hover:bg-purple-100 bg-opacity-75 overflow-hidden
+                    ${active && "ring-2 ring-offset-2 ring-offset-purple-300 ring-white ring-opacity-60 "}
+                    ${checked && "bg-purple-900 border-purple-700   text-white shadow-md hover:bg-purple-800"}
+                    ${disabled && "text-gray-400 "}
+                    ${dense ? "px-1 py-1 " : "px-4 py-3 "}`
+            }
+          >
+            {({ active, checked, disabled }) => (
+              <>
+                {option.Icon && <option.Icon className={`${dense ? "mr-1 p-1" : "mr-2"}`} />}
+                <RadioGroup.Label as="p" className="font-medium truncate flex-grow">
+                  {option.name}
+                </RadioGroup.Label>
+                <div className="flex items-center flex-shrink-0 ml-1">
+                  {showPrice ? (
                     <>
-                      <div className={`flex items-center justify-between w-full `}>
-                        <div className={`flex items-center  truncate mr-0 `}>
-                          {option.Icon && <option.Icon className="mr-2 " />}
-                          <RadioGroup.Label as="p" className="font-medium ">
-                            {option.name}
-                          </RadioGroup.Label>
-                        </div>
-                        <div className="flex items-center">
-                          {showPrice && (
-                            <RadioGroup.Description
-                              as="span"
-                              className={`whitespace-nowrap ${!!checked ? "text-purple-100" : "text-gray-500"}`}
-                            >
-                              {option && option.price && isNaN(option.price) ? option.price : `$ ${option?.price}`}
-                            </RadioGroup.Description>
-                          )}
-                          <div className="flex-shrink-0 text-white w-6 h-6 ml-4">
-                            {checked && <Check className="" />}
-                          </div>
-                        </div>
+                      <RadioGroup.Description
+                        as="span"
+                        className={`whitespace-nowrap ${!!checked ? "text-purple-100" : "text-gray-500"}`}
+                      >
+                        {option && option.price && isNaN(option.price) ? option.price : `$ ${option?.price}`}
+                      </RadioGroup.Description>
+                      <div className="flex-shrink-0 flex items-center text-white w-4 h-4 mx-2">
+                        {checked && <Check className="p-1" />}
                       </div>
                     </>
+                  ) : (
+                    checked && <Check className="p-1" />
                   )}
-                </RadioGroup.Option>
-              ))}
-          </div>
-        </RadioGroup>
+                </div>
+              </>
+            )}
+          </RadioGroup.Option>
+        ))}
       </div>
-    </div>
+    </RadioGroup>
   );
 }
